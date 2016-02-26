8a9c34ecacb12a1b6dd037c066b5ff71c2a76e14
jxy <1013840877@qq.com>
2016-02-26 14:20:15 +0800

    jxyWork创建Person类

---
+++ GitDemo/GitDemo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  GitDemo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 姜晓延. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
