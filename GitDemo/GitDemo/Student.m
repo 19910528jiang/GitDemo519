feabf93563b075447a20ec9d3dfc9857b391216d
jxy <1013840877@qq.com>
2016-02-26 14:25:38 +0800

    jxyWork2创建Student类

---
+++ GitDemo/GitDemo/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  GitDemo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 姜晓延. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
